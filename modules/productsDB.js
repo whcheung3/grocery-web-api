fc9d06b6f56122c371187495bdb38e72dc1113ea
Wing Ho Cheung <whcheung3@myseneca.ca>
2023-01-31 22:49:58 -0500

    first commit

---
+++ modules/productsDB.js
@@ -0,0 +1,94 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  // Universal Product Code, UPC-A barcode contains 12 digits
+  upc: {
+    type: String,
+    minLength: 12,
+    maxLength: 12,
+    unique: true,
+  },
+  // product category, can have more than one category
+  category: [String],
+  // product brand name
+  brand: String,
+  // product name
+  name: String,
+  // product size with unit, e.g. 500ml / 450g / 18lb
+  size: String,
+  // product picture url
+  image: String,
+  // product price history, keep different prices in array
+  history: [
+    {
+      store: String, // store name
+      was_price: Number, // original price
+      price: Number, // sale price
+      valid_to: Date, // price valid date
+    },
+  ],
+});
+
+module.exports = class ProductsDB {
+  constructor() {
+    // We don't have a `Product` object until initialize() is complete
+    this.Product = null;
+  }
+
+  // Pass the connection string to `initialize()`
+  initialize(connectionString) {
+    return new Promise((resolve, reject) => {
+      const db = mongoose.createConnection(connectionString, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+
+      db.once("error", (error) => {
+        reject(error);
+      });
+      db.once("open", () => {
+        this.Product = db.model("products", productSchema);
+        resolve();
+      });
+    });
+  }
+
+  async addNewProduct(data) {
+    const newProduct = new this.Product(data);
+    await newProduct.save();
+    return newProduct;
+  }
+
+  getAllProducts(page, perPage, name) {
+    let findBy = name ? { name } : {};
+
+    if (+page && +perPage) {
+      return this.Product.find(findBy)
+        .sort({ "history.valid_to": -1 })
+        .skip((page - 1) * +perPage)
+        .limit(+perPage)
+        .exec();
+    }
+
+    return Promise.reject(
+      new Error("page and perPage query parameters must be valid numbers")
+    );
+  }
+
+  getProductByUpc(upc) {
+    return this.Product.findOne({ upc: upc }).exec();
+  }
+
+  updateProductByUpc(data, upc) {
+    // return this.Product.updateOne({ upc: upc }, { $set: data }).exec();
+    return this.Product.updateOne(
+      { upc: upc },
+      { $set: { history: { $concatArrays: ["$history", [data]] } } }
+    ).exec();
+  }
+
+  deleteProductByUpc(upc) {
+    return this.Product.deleteOne({ upc: upc }).exec();
+  }
+};
---
+++ server.js
@@ -0,0 +1,101 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+const ProductsDB = require("./modules/productsDB");
+const db = new ProductsDB();
+const HTTP_PORT = process.env.PORT || 8080;
+
+app.use(cors());
+app.use(express.json());
+
+// Confirm server is on
+app.get("/", function (req, res) {
+  res.json({ message: "API Listening" });
+});
+
+// Get all products
+app.get("/api/products", function (req, res) {
+  let queryPromise = null;
+
+  if (req.query.name) {
+    queryPromise = db.getAllProducts(
+      req.query.page,
+      req.query.perPage,
+      req.query.name
+    );
+  } else {
+    queryPromise = db.getAllProducts(req.query.page, req.query.perPage);
+  }
+
+  queryPromise
+    .then((data) => {
+      if (data) res.status(200).json(data);
+      else res.json({ message: "No Products" });
+    })
+    .catch((error) => {
+      res.status(500).json({ message: `Unable to Get All Products: ${error}` });
+    });
+});
+
+// Get one product
+app.get("/api/products/:upc", function (req, res) {
+  db.getProductByUpc(req.params.upc)
+    .then((data) => {
+      if (data) res.status(200).json(data);
+      else res.json({ message: "Product Not Found" });
+    })
+    .catch((error) => {
+      res.status(500).json({ message: `Unable to Get Product: ${error}` });
+    });
+});
+
+// Add new product
+// Expect a JSON object in body, e.g. { "upc": 012345678901, "category": [ "bread" ], "brand": "Wonder" }
+app.post("/api/products", function (req, res) {
+  db.addNewProduct(req.body)
+    .then((data) => {
+      res.status(201).json(data);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: `Unable to Add Product: ${error}` });
+    });
+});
+
+// Update new price history of product
+// Expect a JSON object in body, e.g. { "history": [ { store: "No Frills", was_price: 10.99, price: 9.99, valid_to: "2023-02-04" } ]
+app.put("/api/products/:upc", function (req, res) {
+  db.updateProductByUpc(req.body, req.params.upc)
+    .then(() => {
+      res.status(200).json({ message: `Product Updated: ${req.params.upc}` });
+    })
+    .catch((error) => {
+      res.status(500).json({ message: `Unable to Update Product: ${error}` });
+    });
+});
+
+// Delete product
+app.delete("/api/products/:upc", function (req, res) {
+  db.deleteProductByUpc(req.params.upc)
+    .then(() => {
+      res.status(204).json({ message: `Product Deleted: ${req.params.upc}` });
+    })
+    .catch((error) => {
+      res.status(500).json({ message: `Unable to Delete Product: ${error}` });
+    });
+});
+
+// 404
+app.use((req, res) => {
+  res.status(404).send("Resource not found");
+});
+
+db.initialize(process.env.MONGODB_CONN_STRING)
+  .then(() => {
+    app.listen(HTTP_PORT, () => {
+      console.log(`server listening on: ${HTTP_PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
